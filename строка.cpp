27f4ac04b379b8610813822d00d13408bae99f20
Ikotelevsky <ikotelevsky@gmail.com>
2017-06-01 16:24:04 +0300

    Add files via upload

---
+++ строка.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+void add(string &str, int size = 1) {
+	int temp = str.size() - size;
+	if (temp < 0) {
+		str.insert(0, "1");
+		return;
+	}
+	int ascii = str[str.size() - size];	
+	if (ascii < 57) {
+		str[str.size() - size] = char(ascii + 1);
+	}
+	else if (ascii = 57) {
+		str[str.size() - size] = char(48);
+		size++;
+		add(str,size);
+	}
+}
+
+
+int main() {
+	
+	string str;
+	getline(cin, str);
+	add(str);
+	cout << str;
+	return 0;
+}
+
